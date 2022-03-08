305733028139294b773061f9d263632361aa5734
HelenaHalldinITHS <helena.halldin@iths.se>
2022-03-08 18:50:12 +0100

    Add victim service

---
+++ src/main/java/se/iths/crimedatabase/service/VictimService.java
@@ -0,0 +1,35 @@
+package se.iths.crimedatabase.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.crimedatabase.entity.Victim;
+import se.iths.crimedatabase.repository.VictimRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class VictimService {
+    private final VictimRepository victimRepository;
+
+    public VictimService(VictimRepository victimRepository) {
+        this.victimRepository = victimRepository;
+    }
+
+    public Victim create(Victim victim) {
+        return victimRepository.save(victim);
+    }
+
+    public void delete(Long id) {
+        Victim foundVictim = victimRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        victimRepository.deleteById(foundVictim.getId());
+    }
+
+    public Optional<Victim> findById(Long id) {
+        return victimRepository.findById(id);
+    }
+
+    public Iterable<Victim> findAll() {
+        return victimRepository.findAll();
+    }
+
+}
