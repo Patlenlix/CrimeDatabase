343443ab25f09049dd8f7c92c9c6b3b31ccff5e3
FelixJacobsen <felix.jacobsen@iths.se>
2022-03-08 12:42:19 +0100

    Add Category entity

---
+++ src/main/java/se/iths/crimedatabase/entity/Category.java
@@ -0,0 +1,32 @@
+package se.iths.crimedatabase.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Category setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
