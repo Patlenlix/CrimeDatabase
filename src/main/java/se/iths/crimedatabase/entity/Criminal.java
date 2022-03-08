f6aeec6c88a3eed97f9a06d9778998ec137b7fb6
HelenaHalldinITHS <helena.halldin@iths.se>
2022-03-08 18:20:21 +0100

    Add criminal entity

---
+++ src/main/java/se/iths/crimedatabase/entity/Criminal.java
@@ -0,0 +1,43 @@
+package se.iths.crimedatabase.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Criminal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Criminal setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Criminal setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Criminal setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+}
