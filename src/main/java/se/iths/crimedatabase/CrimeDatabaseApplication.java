e1c3990fc5729bc2859970c4155b3c2cd9dd44a5
FelixJacobsen <felix.jacobsen@iths.se>
2022-03-08 10:27:37 +0100

    Initial commit

---
+++ src/main/java/se/iths/crimedatabase/CrimeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package se.iths.crimedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/crimedatabase/CrimeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.crimedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
