710af0a3d9aca271c19a5852655634bf5997bcc4
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-03-08 15:26:29 +0100

    Add Crime service

---
+++ src/main/java/se/iths/crimedatabase/service/CrimeService.java
@@ -0,0 +1,35 @@
+package se.iths.crimedatabase.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.crimedatabase.entity.Crime;
+import se.iths.crimedatabase.repository.CrimeRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class CrimeService {
+    private final CrimeRepository crimeRepository;
+
+    public CrimeService(CrimeRepository crimeRepository) {
+        this.crimeRepository = crimeRepository;
+    }
+
+    public Crime create(Crime crime) {
+        return crimeRepository.save(crime);
+    }
+
+    public void delete(Long id) {
+        Crime foundCrime = crimeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        crimeRepository.deleteById(foundCrime.getId());
+    }
+
+    public Optional<Crime> findById(Long id) {
+        return crimeRepository.findById(id);
+    }
+
+    public Iterable<Crime> findAll() {
+        return crimeRepository.findAll();
+    }
+
+}
