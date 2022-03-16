7831ec68a985748da95e935a195a8548a9e3da22
HelenaHalldinITHS <helena.halldin@iths.se>
2022-03-16 08:23:08 +0100

    Enable HTTP Basic Authentication

---
+++ src/main/java/se/iths/crimedatabase/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package se.iths.crimedatabase.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Defines datasource for our users
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("user123")).roles("USER");
+    }
+
+
+    //Used to authorize requests
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated() //Authenticated users are authorized to make any request
+                .and()
+                .httpBasic(); //Uses http basic as authentication
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
