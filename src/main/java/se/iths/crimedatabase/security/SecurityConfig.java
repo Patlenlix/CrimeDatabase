21902a5b2f82f1dbf960316afb834424d5440fc8
HelenaHalldinITHS <helena.halldin@iths.se>
2022-03-21 16:01:50 +0100

    Remove unnecessary imports

--- src/main/java/se/iths/crimedatabase/security/SecurityConfig.java
+++ src/main/java/se/iths/crimedatabase/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package se.iths.crimedatabase.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
