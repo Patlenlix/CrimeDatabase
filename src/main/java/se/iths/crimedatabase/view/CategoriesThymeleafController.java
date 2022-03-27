ea47b287612caa35acf254d5d6f1b887a75e9c57
Felix Jacobsen <felix.jacobsen@iths.se>
2022-03-28 01:17:45 +0200

    Add function to add a category in a web browser

--- src/main/java/se/iths/crimedatabase/view/CategoriesThymeleafController.java
+++ src/main/java/se/iths/crimedatabase/view/CategoriesThymeleafController.java
@@ -3,6 +3,8 @@ package se.iths.crimedatabase.view;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import se.iths.crimedatabase.entity.Category;
 import se.iths.crimedatabase.service.CategoryService;
@@ -20,6 +22,20 @@ public class CategoriesThymeleafController {
         Iterable<Category> allCategories = service.findAll();
         mav.addObject("category", allCategories);
         return mav;
+    }
+
+    @GetMapping("/addCategoryForm")
+    public ModelAndView addCategoryForm() {
+        ModelAndView mav = new ModelAndView("add-category-form");
+        Category newCategory = new Category();
+        mav.addObject("category", newCategory);
+        return mav;
+    }
+
+    @PostMapping("/saveCategory")
+    public String saveEmployee(@ModelAttribute Category category) {
+        service.create(category);
+        return "redirect:/showCategories";
     }
 
 }
