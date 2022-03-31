216898d949bfc91456a66b391f66a59ab2e3941b
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-01 00:12:15 +0200

    Disable crsf

--- src/main/java/se/iths/crimedatabase/security/SecurityConfig.java
+++ src/main/java/se/iths/crimedatabase/security/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/showCrimes").hasRole("ADMIN")
                 .antMatchers("/showUsers").hasRole("ADMIN")
