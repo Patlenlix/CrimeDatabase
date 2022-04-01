f3ff2686af3bbfaf2c56280e5be91c71786c8a62
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-01 11:15:03 +0200

    Add LoginPage with authentication

---
+++ src/main/java/se/iths/crimedatabase/view/LoginPage.java
@@ -0,0 +1,22 @@
+package se.iths.crimedatabase.view;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginPage {
+
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken)
+            return "login";
+
+        return "redirect:/";
+    }
+
+}
