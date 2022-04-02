58cf6601493ecb75219e74cfda20046dcbcb9d00
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-02 18:22:04 +0200

    Add method to show addresses

---
+++ src/main/java/se/iths/crimedatabase/view/AddressThymeleafController.java
@@ -0,0 +1,30 @@
+package se.iths.crimedatabase.view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import se.iths.crimedatabase.entity.Address;
+import se.iths.crimedatabase.service.AddressService;
+
+
+@Controller
+public class AddressThymeleafController {
+
+    private final AddressService service;
+
+    @Autowired
+    public AddressThymeleafController(AddressService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/showAddresses")
+    public ModelAndView showAddresses() {
+        ModelAndView mav = new ModelAndView("list-addresses");
+        Iterable<Address> allAddresses = service.findAll();
+        mav.addObject("addresses", allAddresses);
+        return mav;
+    }
+
+
+}
