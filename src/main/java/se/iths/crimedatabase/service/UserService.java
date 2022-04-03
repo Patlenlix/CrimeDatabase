48eaa5add390c9034e609165c19b5bf418ea0261
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-03 17:24:58 +0200

    Add UserService with create, find and delete

---
+++ src/main/java/se/iths/crimedatabase/service/UserService.java
@@ -0,0 +1,36 @@
+package se.iths.crimedatabase.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.crimedatabase.entity.User;
+import se.iths.crimedatabase.repository.UserRepository;
+
+import javax.persistence.EntityExistsException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        User foundUser = userRepository.findById(id).orElseThrow(EntityExistsException::new);
+        userRepository.deleteById(foundUser.getId());
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+ 
+}
