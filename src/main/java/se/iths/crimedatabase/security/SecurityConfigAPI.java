312e29fa4c4f9a1b1ac7596d4eb2277969f09b5a
HelenaHalldinITHS <helena.halldin@iths.se>
2022-04-05 09:05:05 +0200

    Separate security configuration for api and view

--- src/main/java/se/iths/crimedatabase/security/SecurityConfigAPI.java
+++ src/main/java/se/iths/crimedatabase/security/SecurityConfigAPI.java
@@ -1,35 +1,33 @@
 package se.iths.crimedatabase.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
-@Order(2)
+@Order(1)
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfigAPI extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/api/**")
                 .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/images/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .authorizeRequests().anyRequest().hasRole("ADMIN")
+                .and().httpBasic().authenticationEntryPoint(authenticationEntryPoint());
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        BasicAuthenticationEntryPoint entryPoint = new BasicAuthenticationEntryPoint();
+        entryPoint.setRealmName("admin realm");
+        return entryPoint;
     }
 }
