4f4c7b4321719876150dc699645a8a4644811630
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-04-06 22:07:42 +0200

    Change GenerationType.AUTO -> GenerationType.IDENTITY

--- src/main/java/se/iths/crimedatabase/entity/User.java
+++ src/main/java/se/iths/crimedatabase/entity/User.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
