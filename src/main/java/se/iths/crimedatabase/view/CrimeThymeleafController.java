802d5ea560cdb0d2d84dd815ba543bb38a9bb83c
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-06 15:48:05 +0200

    Add showCrimes

--- src/main/java/se/iths/crimedatabase/view/CrimeThymeleafController.java
+++ src/main/java/se/iths/crimedatabase/view/CrimeThymeleafController.java
@@ -2,6 +2,9 @@ package se.iths.crimedatabase.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import se.iths.crimedatabase.entity.Crime;
 import se.iths.crimedatabase.service.AddressService;
 import se.iths.crimedatabase.service.CategoryService;
 import se.iths.crimedatabase.service.CrimeService;
@@ -24,5 +27,14 @@ public class CrimeThymeleafController {
         this.criminalService = criminalService;
     }
 
+
+    @GetMapping("/crimes")
+    public ModelAndView showCrimes() {
+        ModelAndView mav = new ModelAndView("list-crimes");
+        Iterable<Crime> allCrimes = crimeService.findAll();
+        mav.addObject("crimes", allCrimes);
+        return mav;
+    }
+    
 
 }
