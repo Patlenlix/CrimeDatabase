d2025fff428c23d474c85b3e046ab2998b218243
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-04-06 21:50:27 +0200

    Fix datetime-local -> LocalDateTime not working

--- src/main/java/se/iths/crimedatabase/entity/Crime.java
+++ src/main/java/se/iths/crimedatabase/entity/Crime.java
@@ -1,34 +1,31 @@
 package se.iths.crimedatabase.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.context.annotation.Bean;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class Crime {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime time;
-
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     private Category category;
-
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     private Address address;
-
     @ManyToMany
     private Set<Victim> victims = new HashSet<>();
-
     @ManyToMany
     private Set<Criminal> criminals = new HashSet<>();
 
@@ -108,5 +105,10 @@ public class Crime {
     @Override
     public String toString() {
         return name + ", " + time + ", " + category + ", " + address + ", " + victims + ", " + criminals;
+    }
+
+    @Bean
+    public DateTimeFormatter dateTimeFormatter() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     }
 }
